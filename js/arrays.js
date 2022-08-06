c0a954223c2255d863b83ff0f6f88526eee72974
DenysRudenko <heartbreathing19951014@gmail.com>
2022-08-07 00:48:22 +0100

    Finished 2nd homework

---
+++ js/arrays.js
@@ -0,0 +1,15 @@
+// Homework 2
+
+const storeName = "Silpo";
+
+const storeDescription = {
+    budget: 10000,
+    employees: [
+        "Sasha", "Alex", "Igor"
+    ],
+    products: {
+        bread: 15,
+        milk: 10
+    },
+    open: true
+};
