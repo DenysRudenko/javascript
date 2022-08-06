fba1d7b9be61548af44075f7aebac8de3e5545a5
DenysRudenko <heartbreathing19951014@gmail.com>
2022-08-06 23:19:15 +0100

    First home with variables done

---
+++ js/variables.js
@@ -0,0 +1,11 @@
+// This is the first homework for variables
+// Create a variables which will contain name of the user, value = "John". It is constanta!
+// On the second line of code create variable which displays number of the user, value = 25
+// Reset the user number value to 24.
+
+// Solution
+
+const userName = "John";
+let userNumber = 25;
+
+userNumber = 24;
