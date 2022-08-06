fd257ae0b9f31e737d4255819fe8f763c5df5cac
DenysRudenko <heartbreathing19951014@gmail.com>
2022-08-07 00:05:48 +0100

    Numbers,boolean etc.

--- js/script.js
+++ js/script.js
@@ -30,6 +30,33 @@ const vehicleBodyLenght = 4000;
 
 console.log("Width of the car: " + vehicleBodyWidth + ', length: ' + vehicleBodyLenght);
 
-['vruru', 'ecrvrev', 'vvuguev'].map(a => )
+// Numbers
+
+let SecondNumber = 4.6;
+
+console.log(-4 / 0);
+console.log('string' * 9);
+
+const person = "Alex";
+
+const bool = false;
+
+// console.log(something);
+
+let und;
+console.log(und);
+
+const object = {
+    name: "Sasha",
+    age: 25,
+    isMarried: false
+};
+
+console.log(object.name);
+
+let arr = ['plum.png', 'orange.png', 'apple.jpg', 6];
+console.log(arr[1]);
+
+
 
 
