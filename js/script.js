8b271f0baef1985da42b4d9f00e3a128abe828e2
DenysRudenko <heartbreathing19951014@gmail.com>
2022-08-09 15:51:53 +0100

    Simple chat with a user with javascript

--- js/script.js
+++ js/script.js
@@ -1,83 +1,99 @@
 "use strict";
 
-let number = 5;
-const leftBorderWidth = 1;
+// let number = 5;
+// const leftBorderWidth = 1;
 
-number = 10;
-console.log(number);
+// number = 10;
+// console.log(number);
 
-const obj = {
-    a: 50
-};
+// const obj = {
+//     a: 50
+// };
 
-obj.a = 10;
+// obj.a = 10;
 
-console.log(obj);
+// console.log(obj);
 
-// Old example of using variables
+// // Old example of using variables
 
-console.log(name);
-var name = 'Ivan';
+// console.log(name);
+// var name = 'Ivan';
 
-{
-    let result = 50;
-}
+// {
+//     let result = 50;
+// }
 
-// Variables p.2
+// // Variables p.2
 
-const vehicleBodyWidth = 5000;
-const vehicleBodyLenght = 4000;
+// const vehicleBodyWidth = 5000;
+// const vehicleBodyLenght = 4000;
 
-console.log("Width of the car: " + vehicleBodyWidth + ', length: ' + vehicleBodyLenght);
+// console.log("Width of the car: " + vehicleBodyWidth + ', length: ' + vehicleBodyLenght);
 
-// difference between arrays and obj
+// // difference between arrays and obj
 
-const array = [1, 2, 3];
-const arrObj = {
-    0: 'a',
-    1: 'b',
-    2: 'c'
-};
+// const array = [1, 2, 3];
+// const arrObj = {
+//     0: 'a',
+//     1: 'b',
+//     2: 'c'
+// };
 
-// const obj2 = { a: 1, b: 2 };
+// // const obj2 = { a: 1, b: 2 };
 
-console.log(array[1]);
-console.log(arrObj[1]);
+// console.log(array[1]);
+// console.log(arrObj[1]);
 
-const onj2 = {
-    "Anna": 500,
-    "Alice": 800
-};
-
-
-
-// Numbers
-
-let SecondNumber = 4.6;
-
-console.log(-4 / 0);
-console.log('string' * 9);
-
-const person = "Alex";
-
-const bool = false;
-
-// console.log(something);
-
-let und;
-console.log(und);
-
-const object = {
-    name: "Sasha",
-    age: 25,
-    isMarried: false
-};
-
-console.log(object.name);
-
-let arr = ['plum.png', 'orange.png', 'apple.jpg', 6];
-console.log(arr[1]);
+// const onj2 = {
+//     "Anna": 500,
+//     "Alice": 800
+// };
 
 
 
+// // Numbers
+
+// let SecondNumber = 4.6;
+
+// console.log(-4 / 0);
+// console.log('string' * 9);
+
+// const person = "Alex";
+
+// const bool = false;
+
+// // console.log(something);
+
+// let und;
+// console.log(und);
+
+// const object = {
+//     name: "Sasha",
+//     age: 25,
+//     isMarried: false
+// };
+
+// console.log(object.name);
+
+// let arr = ['plum.png', 'orange.png', 'apple.jpg', 6];
+// console.log(arr[1]);
+
+// Simple speaking with a user.
+
+// alert("Heloooooo!");
+
+
+// const result = confirm("Are you here?");
+
+// const answer = prompt("Are you 18?", "");
+// console.log(typeof(answer));
+
+
+const answers = [];
+
+answers[0] = prompt('What is your name?', "");
+answers[1] = prompt('What is your surename?', "");
+answers[2] = prompt('How old are you?', "");
+
+document.write(answers);
 
