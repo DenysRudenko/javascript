8c93da8f3faa2d2d8f4829dbdc7525fe02d7829a
DenysRudenko <heartbreathing19951014@gmail.com>
2022-08-10 21:53:23 +0100

    Functions

--- js/script.js
+++ js/script.js
@@ -371,3 +371,52 @@
 // }
 
 // firstTask();
+
+
+// function
+
+let num = 20;
+
+function showFristMessage(text) {
+    console.log(text);
+    let num = 10;
+
+}
+
+showFristMessage("Hello world!");
+console.log(num);
+
+
+function calc(a, b) {
+    return (a + b);
+}
+
+console.log(calc(4, 3));
+
+
+function ret() {
+    let num = 50;
+
+    //
+
+    return num;
+}
+
+const anotherNum = ret();
+console.log(anotherNum);
+
+
+// function expression
+
+const logger = function () {
+    console.log("hello");  
+};
+
+logger();
+
+const calc1 = (a, b) => a + b;
+
+const cal2 = (a, b) => {
+    console.log('1');
+    return a + b;
+}
