e0d9326bd4851babbd12e99b9cb207a6ec91da87
DenysRudenko <heartbreathing19951014@gmail.com>
2022-08-10 23:38:00 +0100

    Ruturn in Functions

--- js/script.js
+++ js/script.js
@@ -375,48 +375,86 @@
 
 // function
 
-let num = 20;
+// let num = 20;
 
-function showFristMessage(text) {
-    console.log(text);
-    let num = 10;
+// function showFristMessage(text) {
+//     console.log(text);
+//     let num = 10;
+
+// }
+
+// showFristMessage("Hello world!");
+// console.log(num);
+
+
+// function calc(a, b) {
+//     return (a + b);
+// }
+
+// console.log(calc(4, 3));
+
+
+// function ret() {
+//     let num = 50;
+
+//     //
+
+//     return num;
+// }
+
+// const anotherNum = ret();
+// console.log(anotherNum);
+
+
+// // function expression
+
+// const logger = function () {
+//     console.log("hello");  
+// };
+
+// logger();
+
+// const calc1 = (a, b) => a + b;
+
+// const cal2 = (a, b) => {
+//     console.log('1');
+//     return a + b;
+// }
+
+
+// Function arguments
+
+const usdCurr = 28;
+const eurCurr = 32;
+const discount = 0.9;
+
+function convert(amount, curr) {
+    return curr * amount;
+}
+
+function promotion(result) {
+    console.log( result * discount );
 
 }
 
-showFristMessage("Hello world!");
-console.log(num);
 
+const res = convert(500, usdCurr);
+promotion(res);
 
-function calc(a, b) {
-    return (a + b);
+function test() {
+    for (let i = 0; i < 5; i++){
+        console.log(i);
+        if (i === 3) return
+    }
+    console.log("Done");
 }
 
-console.log(calc(4, 3));
+test();
 
 
-function ret() {
-    let num = 50;
+// Empty function!!!
 
-    //
-
-    return num;
-}
-
-const anotherNum = ret();
-console.log(anotherNum);
-
-
-// function expression
-
-const logger = function () {
-    console.log("hello");  
+function doNothing() {
 };
 
-logger();
-
-const calc1 = (a, b) => a + b;
-
-const cal2 = (a, b) => {
-    console.log('1');
-    return a + b;
-}
+console.log(doNothing() === undefined);
